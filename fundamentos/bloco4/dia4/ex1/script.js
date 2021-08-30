5a27330f1f728108df113bf826f367bd1e4ac439
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2021-08-30 19:50:38 -0300

    adicionado mais algumas funcionalidadades

--- fundamentos/bloco4/dia4/ex1/script.js
+++ fundamentos/bloco4/dia4/ex1/script.js
@@ -1,9 +1,13 @@
-let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
 
-  for (let key in car) {
-      console.log(key, car[key]);
-  }
+
+let Mat = {
+    soma: (a,b)=>{return a+b},
+    subtracao: (a,b)=>{return a-b},
+    multiplicacao: (a,b)=>{return a*b},
+    divisao: (a,b)=>{return a/b}
+};
+
+let idade = 20;
+let monthsAlive = Mat.multiplicacao(20,12);
+
+console.log(monthsAlive);
