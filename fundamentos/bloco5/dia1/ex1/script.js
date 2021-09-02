aa88f15ce4ff216522af3b1380c617d72ca9eaa7
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2021-09-02 16:08:38 -0300

    ex1 adicionado

---
+++ fundamentos/bloco5/dia1/ex1/script.js
@@ -0,0 +1,17 @@
+const cabeçalho = document.querySelector('header');
+const titulos = document.querySelectorAll("h3");
+const div = document.querySelectorAll("section");
+const footer = document.querySelector("#footer-container");
+
+
+cabeçalho.style = 'background-color:green';
+
+titulos[0].style = 'background-color:purple';
+titulos[1].style = 'background-color:purple';
+titulos[2].style = 'background-color:black';
+titulos[3].style = 'background-color:black';
+
+div[0].style = 'background-color:salmon';
+div[1].style = 'background-color:yellow';
+
+footer.style = 'background-color:black';
