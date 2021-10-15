a7191a41fde9e08e83d419e15227fd671d7328a4
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2021-10-15 14:54:00 -0300

    adicionado bloco9 ao repositorio

---
+++ fundamentos/bloco8/dia4/script.js
@@ -0,0 +1,5 @@
+let array = [1, 2, ,3 ,4 ,5, 6, 7, 8];
+
+const sumArray = array.reduce((current, number) => current + number);
+
+console.log(sumArray);
--- fundamentos/dia2/script.js
+++
@@ -1,5 +0,0 @@
-let array = [1, 5, 8, 2, 6, 9, 15];
-
-array.sort((a, b) => b - a);
-
-console.table(array);
